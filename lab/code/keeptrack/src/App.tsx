4822b1ad6d646c571c6775b7c84fd3cb63efdc9f
vasantbala <30379760+vasantbala@users.noreply.github.com>
2024-08-10 17:00:27 +0000

    react and ts half way

--- lab/code/keeptrack/src/App.tsx
+++ lab/code/keeptrack/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ProjectsPage from './projects/ProjectsPage'
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="container">
       <ProjectsPage/>
+      <Hello name="David"/>
+      <Hello name="Warner" enthusiasmLevel={3}/>
+      <Hello name="Tim" enthusiasmLevel={0}/>
     </div>
   );
 }
